b40c50d2bd2bcfecb953750d2c33bb8892799a5c
Kawshi Bandara <kawshi@geveo.com>
2021-01-08 17:20:05 +0530

    Add dashboard UI test and reset password test prep methods.

---
+++ CCS_UI_Automation/CCS.UITests.Web.MainTests/DashboardTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCS.UITests.Web.MainTests
+{
+    [TestFixture, Order(1)]
+    public class DashboardTest : BaseTest
+    {
+
+        [SetUp]
+        //Open up the url in browser and login to the system
+        public void Login()
+        {
+            WebGlobal.StartupPortal();
+
+        }
+
+
+        //Verify the Dashboard page UI functionalities
+        [Test, Order(3)]
+        public void VerifyDashboardPage()
+        {
+
+
+        }
+
+        //Verify the SSO page launch functionality
+        [Test, Order(4)]
+        public void VerifySSOLaunch()
+        {
+
+
+        }
+    }
+}
---
+++ CCS_UI_Automation/CCS.UITests.Web.MainTests/ResetPasswordTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCS.UITests.Web.MainTests
+{
+    [TestFixture, Order(1)]
+    public class ResetPasswordTest : BaseTest
+    {
+        [SetUp]
+        //Open up the url and login to the system
+        public void Login()
+        {
+            WebGlobal.StartupPortal();
+
+        }
+
+        //Verify the reset password UI functionalities
+        [Test, Order(5)]
+        public void VerifyResetPassword()
+        {
+
+
+        }
+
+    }
+}
